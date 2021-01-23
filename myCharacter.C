2437aeef78f8006764317da710d54759af7a127b
Alexa Mendoza <77183059+AlMen02@users.noreply.github.com>
2021-01-23 09:16:12 -0800

    Create myCharacter.C

---
+++ myCharacter.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("By Alexa M.");printf("\n");
+        printf("--------------------");printf("\n");
+        printf("|Follow my commands!|  ");printf("\n");
+        printf("--------------------");printf("\n");
+        printf(" |");printf("\n");
+        printf("  _ - ");printf("\n");
+        printf("//o-o\\\\");printf("\n");
+        printf(" <( )>");printf("\n");
+        printf("  / |");printf("\n");
+        printf ("        -----------");printf("\n");
+        printf ("        |  Ruff!   |");printf("\n");
+        printf ("        ------------");printf("\n");
+        printf ("            |       ");printf("\n");
+        printf ("        ^---^");printf("\n");
+        printf ("        \\o.o/");printf("\n");
+        printf ("         -------_");printf("\n");
+        printf ("          ||  ||");printf("\n");
+
+
+
+
+//n
+
+
+return 0;
+
+
+}
